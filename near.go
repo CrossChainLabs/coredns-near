de66a936a257cf2ae9c9641eb8842672687c22fe
aliasccl <george@crosschainlabs.tech>
2021-04-15 17:49:58 +0300

    Corefile rewrite, log state

--- near.go
+++ near.go
@@ -271,6 +271,7 @@ func (n NEAR) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (i
 	fmt.Println("near result:", res, "logs:", resp.Logs, "err:", err)
 
 	state := request.Request{W: w, Req: r}
+	fmt.Println("state: %+v", state)
 
 	a := new(dns.Msg)
 	a.SetReply(r)
