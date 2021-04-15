302af440b52cf83aee9e953e7ace0cd436080fc6
aliasccl <george@crosschainlabs.tech>
2021-04-15 10:22:24 +0300

    use Example plugin

--- setup_test.go
+++ setup_test.go
@@ -1,18 +1,20 @@
-package simple
+package example
 
 import (
 	"testing"
 
-	"github.com/caddyserver/caddy"
+	"github.com/coredns/caddy"
 )
 
+// TestSetup tests the various things that should be parsed by setup.
+// Make sure you also test for parse errors.
 func TestSetup(t *testing.T) {
-	c := caddy.NewTestController("dns", `simple`)
+	c := caddy.NewTestController("dns", `example`)
 	if err := setup(c); err != nil {
 		t.Fatalf("Expected no errors, but got: %v", err)
 	}
 
-	c = caddy.NewTestController("dns", `simple example.org`)
+	c = caddy.NewTestController("dns", `example more`)
 	if err := setup(c); err == nil {
 		t.Fatalf("Expected errors, but got: %v", err)
 	}
